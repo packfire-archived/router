66f930033e02347f5151a93090a05138a5744ebe
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-12 10:05:17 +0800

    request interface

---
+++ src/Packfire/Router/RequestInterface.php
@@ -0,0 +1,18 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router;
+
+interface RequestInterface
+{
+    public function __construct($host, $verb, $path);
+
+    public function path();
+
+    public function verb();
+
+    public function host();
+}
