59d57b8b1f36f50f26d042a367b3681317a39a7c
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-17 22:08:13 +0800

    current request path determination

--- src/Packfire/Router/CurrentRequest.php
+++ src/Packfire/Router/CurrentRequest.php
@@ -8,16 +8,38 @@ namespace Packfire\Router;
 
 class CurrentRequest implements RequestInterface
 {
+    protected $path;
+
     public function __construct($server = null)
     {
         if (!$server) {
             $server = $_SERVER;
         }
+        $this->path = $this->determinePath($server);
+    }
+
+    protected function determinePath($server)
+    {
+        $path = null;
+        if (isset($server['ORIG_PATH_INFO'])) {
+            $path = $server['ORIG_PATH_INFO'];
+        } elseif (isset($server['PATH_INFO'])) {
+            $path = $server['PATH_INFO'];
+        } else {
+            $scriptName = $server['SCRIPT_NAME'];
+            $phpSelf = $server['PHP_SELF'];
+            if ($scriptName == $phpSelf) {
+                $path = '/';
+            } else {
+                $path = substr($phpSelf, strlen($scriptName));
+            }
+        }
+        return $path;
     }
 
     public function path()
     {
-
+        return $this->path;
     }
 
     public function method()
