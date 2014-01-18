29296df69af3e59925fc94015bead5d345c60b21
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-19 01:15:47 +0800

    baseroute implementation

---
+++ src/Packfire/Router/Routes/BaseRoute.php
@@ -0,0 +1,40 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router\Routes;
+
+use Packfire\Router\RouteInterface;
+
+class BaseRoute implements RouteInterface
+{
+    protected $name;
+
+    protected $rules;
+
+    protected $config;
+
+    public function __construct($name, $rules, $config)
+    {
+        $this->name = $name;
+        $this->rules = $rules;
+        $this->config = $config;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function rules()
+    {
+        return $this->rules;
+    }
+
+    public function callback()
+    {
+        
+    }
+}
