4b605b7a306822d0c57ceef2a6ed63232612877a
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-25 10:07:17 +0800

    added pathmatcher test

---
+++ test/Packfire/Router/Matchers/PathMatcherTest.php
@@ -0,0 +1,38 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router\Matchers;
+
+use \PHPUnit_Framework_TestCase;
+use Packfire\Router\CurrentRequest;
+use Packfire\Router\Routes\BaseRoute;
+
+class CurrentRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testMatch()
+    {
+        $request = new CurrentRequest(
+            array(
+                'SCRIPT_NAME' => '/index.php',
+                'PHP_SELF' => '/index.php/test/example/10.json.xml'
+            )
+        );
+
+        $matcher = new PathMatcher($request);
+
+        $route = new BaseRoute(
+            'test',
+            array(
+                'path' => array(
+                    'uri' => '/test/example/:id(.:format?)'
+                )
+            ),
+            array()
+        );
+
+        $this->assertTrue($matcher->match($route));
+    }
+}
