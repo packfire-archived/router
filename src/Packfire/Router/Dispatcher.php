8de05cde358efc8edc06c29613f8229fe6ef2a63
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-26 00:13:01 +0800

    added implementation for dispatcher

--- src/Packfire/Router/Dispatcher.php
+++ src/Packfire/Router/Dispatcher.php
@@ -8,8 +8,35 @@ namespace Packfire\Router;
 
 class Dispatcher implements DispatcherInterface
 {
-    public function call($instance, $action, $params = array())
+    public function dispatch($action, $params = array())
     {
-        
+        $invokeParams = array();
+        if (is_array($action)) {
+            $reflection = new \ReflectionMethod($action[0], $action[1]);
+            if ($reflection->isStatic()) {
+                $invokeParams[] = null;
+            } else {
+                $invokeParams[] = $action[0];
+            }
+        } else {
+            $reflection = new \ReflectionFunction($action);
+        }
+
+        $pass = array();
+        foreach ($reflection->getParameters() as $param) {
+            /* @var $param ReflectionParameter */
+            if (isset($params[$param->getName()])) {
+                $pass[] = $params[$param->getName()];
+            } elseif ($param->isOptional()) {
+                try {
+                    $pass[] = $param->getDefaultValue();
+                } catch (\ReflectionException $ex) {
+
+                }
+            }
+        }
+        $invokeParams[] = $pass;
+
+        return call_user_func_array(array($reflection, 'invokeArgs'), $invokeParams);
     }
 }
