f3b091c82caeddda74dc825a378c49254809a91c
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-26 00:00:19 +0800

    dispatcher class added

---
+++ src/Packfire/Router/Dispatcher.php
@@ -0,0 +1,15 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router;
+
+class Dispatcher implements DispatcherInterface
+{
+    public function call($instance, $action, $params = array())
+    {
+        
+    }
+}
