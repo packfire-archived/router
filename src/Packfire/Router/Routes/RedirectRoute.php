b5747893080b622c799da94a3967a71fd8d7cc0d
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-29 00:09:58 +0800

    added redirectroute

---
+++ src/Packfire/Router/Routes/RedirectRoute.php
@@ -0,0 +1,24 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router\Routes;
+
+use Packfire\FuelBlade\ConsumerInterface;
+use Packfire\Router\Dispatcher;
+
+class RedirectRoute extends AbstractRoute implements ConsumerInterface
+{
+    protected $container;
+
+    public function callback()
+    {
+        $target = $this->config['target'];
+        $code = isset($this->config['http']) ? (int)$this->config['http'] : 302;
+
+        header('Location: ' . $target, true, $code);
+        exit;
+    }
+}
