b1193534916fd1977294449d7b3cecc9a8db7f14
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-29 10:13:53 +0800

    added test for router

---
+++ test/Packfire/Router/RouterTest.php
@@ -0,0 +1,34 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router;
+
+use \PHPUnit_Framework_TestCase;
+
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $config = array(
+            'path' => '/test',
+            'target' => 'http://heartcode.sg/'
+        );
+
+        $router = new Router();
+        $router->add('test', $config);
+
+        $request = new CurrentRequest(
+            array(
+                'SCRIPT_NAME' => '/index.php',
+                'PHP_SELF' => '/index.php/test'
+            )
+        );
+
+        $route = $router->route($request);
+        $this->assertInstanceOf('Packfire\\Router\\RouteInterface', $route);
+        $this->assertEquals('test', $route->name());
+    }
+}
