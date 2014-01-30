550703e6e6f5d2656a449bfbbf22bff7014e3aa5
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-30 08:56:46 +0800

    added test for generator

---
+++ test/Packfire/Router/GeneratorTest.php
@@ -0,0 +1,31 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router;
+
+use \PHPUnit_Framework_TestCase;
+use Packfire\Router\Routes\BaseRoute;
+
+class GeneratorTest extends PHPUnit_Framework_TestCase
+{
+    public function testGenerate()
+    {
+        $route = new BaseRoute(
+            'test',
+            array(
+                'path' => '/test/example/:id(.:format?)',
+                'params' => array(
+                    'id' => 'i'
+                )
+            )
+        );
+
+        $generator = new Generator();
+
+        $uri = $generator->generate($route, array('id' => 4));
+        $this->assertEquals('/test/example/4', $uri);
+    }
+}
