d0b77011d9e4e9a82ad0f0072dc5f4af33a84e13
Sam-Mauris Yong <mauris@hotmail.sg>
2014-02-05 08:48:54 +0800

    added redirectroutetest

---
+++ test/Packfire/Router/Routes/RedirectRouteTest.php
@@ -0,0 +1,33 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Router project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Router\Routes;
+
+use \PHPUnit_Framework_TestCase;
+use Packfire\FuelBlade\Container;
+
+function header($header, $replacement = false)
+{
+
+}
+
+class RedirectRouteTest extends PHPUnit_Framework_TestCase
+{
+    public function testCallback()
+    {
+        $container = new Container();
+        $container['PHPUnit_Framework_TestCase'] = $this;
+
+        $config = array(
+            'path' => '/blog',
+            'target' => 'http://blog.example.com'
+        );
+        $route = $container->instantiate('Packfire\\Router\\Routes\\RedirectRoute', array('name' => 'test', 'config' => $config));
+
+        $this->setExpectedException('\\RuntimeException');
+        $route->callback();
+    }
+}
