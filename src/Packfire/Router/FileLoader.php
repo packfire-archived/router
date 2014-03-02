dbfc72b94209ff6b36ad55e8f490c971a9d58bc9
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 09:21:52 +0800

    in-code docs for fileloader

--- src/Packfire/Router/FileLoader.php
+++ src/Packfire/Router/FileLoader.php
@@ -12,16 +12,34 @@ use Packfire\Config\ConfigFactory;
 
 class FileLoader implements LoaderInterface, ConsumerInterface
 {
+    /**
+     * The IoC Container
+     * @var Packfire\FuelBlade\ContainerInterface
+     */
     protected $container;
 
+    /**
+     * The file to load from
+     * @var string
+     */
     protected $file;
 
+    /**
+     * Create a new FileLoader object
+     * @param string $file The pathname to the file
+     * @return void
+     */
     public function __construct($file)
     {
         $this->file = $file;
         $this->container = new Container();
     }
 
+    /**
+     * Load the configuration to a router
+     * @param  Packfire\Router\RouterInterface $router (optional) Provide a specific router to load the configuration into
+     * @return Packfire\Router\RouterInterface Returns a router loaded with the configuration
+     */
     public function load(RouterInterface $router = null)
     {
         if (!$router) {
@@ -43,8 +61,14 @@ class FileLoader implements LoaderInterface, ConsumerInterface
         return $router;
     }
 
+    /**
+     * Inject the FuelBlade IoC Container
+     * @param  Packfire\FuelBlade\ContainerInterface $container The container of dependencies to inject
+     * @return Packfire\Router\FileLoader Returns self
+     */
     public function __invoke($container)
     {
         $this->container = $container;
+        return $this;
     }
 }
