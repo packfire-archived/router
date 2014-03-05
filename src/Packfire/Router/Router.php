2e9c1e3f26ff67e6e5abee420b75ff2aec8302f6
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-05 23:15:19 +0800

    added in code docs for Router

--- src/Packfire/Router/Router.php
+++ src/Packfire/Router/Router.php
@@ -12,23 +12,50 @@ use Packfire\Router\Exceptions\RouteNotFoundException;
 
 class Router implements RouterInterface, ConsumerInterface
 {
+    /**
+     * The array of routes in the router
+     * @var array
+     */
     protected $routes = array();
 
+    /**
+     * The FuelBlade IoC container
+     * @var Packfire\FuelBlade\ContainerInterface
+     */
     protected $container;
 
+    /**
+     * The RouteFactory to use
+     * @var Packfire\Router\RouteFactoryInterface
+     */
     protected $factory;
 
+    /**
+     * Create a new Router object
+     * @return void
+     */
     public function __construct()
     {
         $this->container = new Container();
         $this->factory = $this->container->instantiate('Packfire\\Router\\RouteFactory');
     }
 
+    /**
+     * Add a new route to the router
+     * @param string $name Name of the route to add into the router
+     * @param array  $config The configuration of the route
+     * @return void
+     */
     public function add($name, $config = array())
     {
         $this->routes[$name] = $this->factory->create($name, $config);
     }
 
+    /**
+     * Route a request to its action
+     * @param Packfire\Router\RequestInterface $request The request to route
+     * @return Packfire\Router\RouteInterface Returns the matching route of the request
+     */
     public function route(RequestInterface $request)
     {
         if (isset($this->container['Packfire\\Router\\MatcherInterface'])) {
@@ -60,6 +87,12 @@ class Router implements RouterInterface, ConsumerInterface
         return null;
     }
 
+    /**
+     * Generate a URL for a route with its parameters
+     * @param  string $name Name of the route in the router to generate
+     * @param  array  $params (optional) The optional parameters to enter into the URL
+     * @return string Returns the generated URL of the route and its parameters
+     */
     public function generate($name, $params = array())
     {
         if (isset($this->routes[$name])) {
@@ -74,6 +107,11 @@ class Router implements RouterInterface, ConsumerInterface
         }
     }
 
+    /**
+     * Inject the object with the IoC container
+     * @param  Packfire\FuelBlade\ContainerInterface|array $container The FuelBlade IoC Container
+     * @return Packfire\Router\Routes\BaseRoute Returns self
+     */
     public function __invoke($container)
     {
         $this->container = $container;
